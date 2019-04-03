7f1bd148cfbcc89f4bbdee5da392aa195ce6eb65
Jim Gibbs <jamesvgibbs@gmail.com>
2019-04-03 16:17:01 -0400

    the unpinning didn't work--- should be fixed now

--- NewComputerSetup/Windows10Debloater.ps1
+++ NewComputerSetup/Windows10Debloater.ps1
@@ -2,7 +2,7 @@
 
 $runningAsAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
-if(-not $runningAsAdmin){
+if (-not $runningAsAdmin) {
     throw 'Rerun this script as an admin'
 }
 
@@ -922,61 +922,46 @@ function UninstallOtherSoftware {
     
 }
 
-
-function Pin-App {
-    param(
-        [string]$appname,
-        [switch]$unpin
-    )
-    try {
-        if ($unpin.IsPresent) {
-            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $appname }).Verbs() | Where-Object { $_.Name.replace('&', '') -match 'From "Start" UnPin|Unpin from Start' } | ForEach-Object { $_.DoIt() }
-            Write-Output "App '$appname' unpinned from Start"
-        }
-        else {
-            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $appname }).Verbs() | Where-Object { $_.Name.replace('&', '') -match 'To "Start" Pin|Pin to Start' } | ForEach-Object { $_.DoIt() }
-            Write-Output  "App '$appname' pinned to Start"
-        }
-    }
-    catch {
-        Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
-    }
-}
-
-
 function unPinJunk {
-    Pin-App "Mail" -unpin
-    Pin-App "Store" -unpin
-    Pin-App "Calendar" -unpin
-    Pin-App "Microsoft Edge" -unpin
-    Pin-App "Photos" -unpin
-    Pin-App "Cortana" -unpin
-    Pin-App "Weather" -unpin
-    Pin-App "Phone Companion" -unpin
-    Pin-App "Music" -unpin
-    Pin-App "xbox" -unpin
-    Pin-App "movies & tv" -unpin
-    Pin-App "microsoft solitaire collection" -unpin
-    Pin-App "money" -unpin
-    Pin-App "get office" -unpin
-    Pin-App "onenote" -unpin
-    Pin-App "news" -unpin
-    Pin-App "Mail" -pin
-    Pin-App "Store" -pin
-    Pin-App "Calendar" -pin
-    Pin-App "Microsoft Edge" -pin
-    Pin-App "Photos" -pin
-    Pin-App "Cortana" -pin
-    Pin-App "Weather" -pin
-    Pin-App "Phone Companion" -pin
-    Pin-App "Music" -pin
-    Pin-App "xbox" -pin
-    Pin-App "movies & tv" -pin
-    Pin-App "microsoft solitaire collection" -pin
-    Pin-App "money" -pin
-    Pin-App "get office" -pin
-    Pin-App "onenote" -pin
-    Pin-App "news" -pin
+    $items = @(
+        "Mail"
+        "Store"
+        "Calendar"
+        "Microsoft Edge"
+        "Photos"
+        "Cortana"
+        "Weather"
+        "Phone Companion"
+        "Music"
+        "xbox"
+        "movies & tv"
+        "microsoft solitaire collection"
+        "money"
+        "get office"
+        "onenote"
+        "news"
+        "Mail"
+        "Store"
+        "Calendar"
+        "Microsoft Edge"
+        "Photos"
+        "Cortana"
+        "Weather"
+        "Phone Companion"
+        "Music"
+        "xbox"
+        "movies & tv"
+        "microsoft solitaire collection"
+        "money"
+        "get office"
+        "onenote"
+        "news"
+    )
+
+    $items | ForEach-Object {
+        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object { $_.Name -eq $appname }).Verbs() | Where-Object { $_.Name.replace('&', '') -match 'From "Start" UnPin|Unpin from Start' } | ForEach-Object { $_.DoIt() }
+        Write-Output "App '$appname' unpinned from Start"
+    }
 }
 
 function TakeownRegistry($key) {
